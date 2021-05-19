a1f87ab249e95781797d492381ed30468f0097b1
Diegu <diegudeveloper@gmail.com>
2021-05-19 23:25:18 +0200

    Borrar el docigo anterior de javascript

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/*const btnAboutme = document.getElementById("btn-aboutme");
-    btnAboutme.addEventListener("click", sliderAbautMe);
-    btnHome = document.getElementById("btn-home");
-    btnHome.addEventListener("click", sliderhome);
-    btnResumen = document.getElementById("btn-resume");
-    btnResumen.addEventListener("click", sliderresumen);
-
-function sliderAbautMe () {
-    let infoabout = document.getElementById("infabout");
-    //infoabout.style.transform = "translatex(0)"
-    infoabout.style.left=(0);
-}
-
-function sliderhome () {
-    let infohome = document.getElementById("infohome");
-    //infoabout.style.transform = "translatex(0)"
-    infohome.style.left=(100);
-}
-function sliderresumen () {
-    let inforesumen = document.getElementById("inforesumen");
-    //infoabout.style.transform = "translatex(0)"
-    inforesumen.style.left=(0);
-}*/
-
 document.addEventListener('DOMContentLoaded', () => {
 	const btnHome = document.getElementById('btn-home');
 	const btnAbout = document.getElementById('btn-about');
