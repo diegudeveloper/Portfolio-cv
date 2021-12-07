0534dca22521a66cd94d7d98e8a91ddf63151e7a
Diegudeveloper <diegudeveloper@gmail.com>
2021-12-07 20:03:35 +0100

    configuracion de la foto del header

--- script.js
+++ script.js
@@ -62,3 +62,38 @@ document.addEventListener('DOMContentLoaded', () => {
 		// console.log('open', btnContact, divContact);
 	});
 });
+
+
+// async function getContributions(token, username) {
+//     const headers = {
+//         'Authorization': `bearer ${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}`,
+//     }
+//     const body = {
+//         "query": `query {
+//             user(login: "${username}") {
+//               name
+//               contributionsCollection {
+//                 contributionCalendar {
+//                   colors
+//                   totalContributions
+//                   weeks {
+//                     contributionDays {
+//                       color
+//                       contributionCount
+//                       date
+//                       weekday
+//                     }
+//                     firstDay
+//                   }
+//                 }
+//               }
+//             }
+//         }`
+//     }
+//     const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers })
+//     const data = await response.json()
+//     return data
+// }
+
+// const data = await getContributions('token', 'diegudeveloper')
+// console.log(data)
