36a26bb45039f3917f022e7e62b65bb88e1623df
Diegu <diegudeveloper@gmail.com>
2022-06-10 16:24:34 +0200

    Se declara la variable puntos, tiempo y necesarios para el juego

--- script.js
+++ script.js
@@ -117,9 +117,9 @@ document.addEventListener('DOMContentLoaded', () => {
 // juego
 document.getElementById('player').addEventListener("mouseover",sumarPuntos);
 
-puntos = 0;
-tiempo = 60;
-necesarios = 30;
+let puntos = 0;
+let tiempo = 60;
+let necesarios = 30;
 function sumarPuntos(){
     puntos++;
     document.getElementById("puntos").innerHTML = "Puntos: <b>" + puntos + "/" + necesarios + "  </b>";
