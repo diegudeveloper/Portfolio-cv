07cb9d0a785a3046892562af038c70a1f17ad462
Diegu <diegudeveloper@gmail.com>
2022-06-10 16:59:23 +0200

    Conf de javascript para el juego

--- script.js
+++ script.js
@@ -121,10 +121,11 @@ let puntos = 0;
 let tiempo = 60;
 let necesarios = 30;
 function sumarPuntos(){
+
     puntos++;
     document.getElementById("puntos").innerHTML = "Puntos: <b>" + puntos + "/" + necesarios + "  </b>";
-    randNum =  Math.round(Math.random()*500);
-    randNum2 =  Math.round(Math.random()*500);
+    let randNum =  Math.round(Math.random()*500);
+    let  randNum2 =  Math.round(Math.random()*500);
     document.getElementById("player").style.marginTop =randNum + "px";
     document.getElementById("player").style.marginLeft =randNum2 + "px";
     if (puntos == 30) {
